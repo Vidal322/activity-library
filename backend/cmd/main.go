d6d4906d7332ee003d80ba474ec1db68a88c5517
Pedro Vidal <up202108754@up.pt>
2026-08-23 10:50:11 +0100

    working api

---
+++ backend/cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+)
+
+func main() {
+	cfg := config{
+		addr: ":8080",
+		db:   dbConfig{},
+	}
+	api := application{
+		config: cfg,
+	}
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	err := api.run(api.mount(cfg))
+	if err != nil {
+		slog.Error("Server failed to start", "error", err)
+		os.Exit(1)
+	}
+}
